7fce4d2a6469dc6c34cb62cb4722b6b7359e841c
Max213141 <66839835+Max213141@users.noreply.github.com>
2020-10-10 22:42:23 +0300

    JS components was created. Creating of component and aditing of classes was automatizated

---
+++ src/components/toolbar/Toolbar.js
@@ -0,0 +1,32 @@
+import {ExcelComponent} from "@core/ExcelComponent";
+
+export class Toolbar extends ExcelComponent {
+    static className = 'excel__toolbar'
+    toHTML() {
+        return `
+            <div class="button">
+               <i class="material-icons">format_align_justify</i>
+               </div>
+
+            <div class="button">
+              <i class="material-icons">format_align_right</i>
+            </div>
+
+            <div class="button">
+              <i class="material-icons">format_align_center</i>
+            </div>
+
+            <div class="button">
+              <i class="material-icons">format_bold</i>
+            </div>
+
+            <div class="button">
+              <i class="material-icons">format_italic</i>
+            </div>
+
+            <div class="button">
+              <i class="material-icons">format_underlined</i>
+            </div>
+        `
+    }
+}
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,8 @@
+import {DomListener} from '@core/Domlistener';
+
+export class ExcelComponent extends DomListener {
+  // Return Template of the component
+  toHTML() {
+    return ``
+  }
+}
